903bdbeffd5769ac063389d82b3acaabcc77e054
Goak <goaktefp@gmail.com>
2021-06-18 11:35:42 -0300

    Microdesafio aula 7 - Arrow Functions

---
+++ md1_aula7_arrowfunction.js
@@ -0,0 +1,25 @@
+// function print(mensagem) {
+//     console.log(mensagem)
+// }
+
+// print('Olá, bom dia')
+
+// function soma(n1, n2) {
+//     return n1 + n2
+// }
+
+// console.log(soma(10,10))
+
+let print = mensagem => console.log(mensagem);
+
+print('Olá,bom dia');
+
+// let soma = (n1, n2) => {
+//     return n1 + n2
+// }
+
+// console.log(soma(5, 5));
+
+let soma=(n1,n2)=>n1+n2
+
+console.log(soma(15,25));
